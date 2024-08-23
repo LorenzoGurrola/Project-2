3bc89ab0619dcdc5c0f0a151b8d1b776ce14adec
LorenzoGurrola <lorenzogurrola521@gmail.com>
2024-08-23 17:12:06 -0500

    implement back prop -not working-

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -8,12 +8,34 @@ if True:
     import sys
     sys.path.insert(
         0, 'C:/Users/Lorenzo/Desktop/Workspace/Github/Project-2/src')
-    from train_model import normalize, initialize_parameters, forward_propagation, calculate_cost, back_propagation, update_parameters
+    from train_model import initialize_params, forward_prop, calculate_cost, back_prop
 
 
-class test_normalize(unittest.TestCase):
+class test_forward_and_back_prop(unittest.TestCase):
 
     def test_basic(self):
-        pass
+        m = 80
+        x = np.random.randn(m, 1)
+        y = np.random.randint(0, 2, (m, 1))
+        params = initialize_params()
+        yhat, inter_vals = forward_prop(x, params)
+        cost = calculate_cost(yhat, y)
+        grads = back_prop(x, yhat, y, inter_vals)
+        dw_result = grads['dw']
+        db_result = grads['db']
+
+        x = torch.tensor(x)
+        y = torch.tensor(y)
+        w, b = torch.tensor(params['w'], requires_grad=True), torch.tensor(params['b'], requires_grad=True)
+        yhat = x @ w + b
+        losses = (y * torch.log(yhat)) + ((1 - y) * torch.log(1 - yhat))
+        cost = -torch.sum(losses, dim=0, keepdims=True)/m
+        cost.backward()
+        dw_expected = w.grad.detach().numpy()
+        db_expected = b.grad.detach().numpy()
+        
+        np.testing.assert_allclose(dw_result, dw_expected)
+        np.testing.assert_allclose(db_result, db_expected)
+
 
 unittest.main()
